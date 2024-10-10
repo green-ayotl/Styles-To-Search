c999473e3de03a40be37443ea599ab4e8ccc053d
Eduardo Castellanos <141962386+green-ayotl@users.noreply.github.com>
2024-10-10 14:13:28 -0600

    Update styles_to_search - Amazon - ASIN.R
    Cambios en script de Amazon

--- R - Scripts/styles_to_search - Amazon - ASIN.R
+++ R - Scripts/styles_to_search - Amazon - ASIN.R
@@ -12,7 +12,7 @@ tamaño <- "1600x1600"
 alto <- "1600" # Canvas size
 ancho <- "1600" #Canvas size
 dpi <- 72
-extension <- ".jpeg"
+extension <- ".jpg"
 
 #Lista de archivos
 styles_to_search <- read_excel("Styles To Search - General.xlsx", sheet = "Amazon - IMG")
@@ -41,12 +41,12 @@ for (i in 1:nrow(styles_to_search)) {
   full_name <- styles_to_search$`Full Name`[i]
   IMG <- image_read( path = full_name) |> image_trim(fuzz = 20) |> image_scale(tamaño)
   IMG <- image_composite(canvas, IMG, gravity = "Center")
-  image_write(IMG, path = paste0(carpeta_final,"/",styles_to_search$Rename[i],extension), density = dpi)
+  image_write(IMG, path = paste0(carpeta_final,"/",styles_to_search$Rename[i]), format = extension , density = dpi)
   print(paste0(styles_to_search$Rename[i], "; procesado: ", counting," de ", total_imgs))
   counting <- counting + 1
 }
 
-#Archivo ZIP
+#Archivo ZIP - Testing
 comprimir <- readline( prompt = "Comprimir imagenes en archivo ZIP [Y]")
 if(comprimir == "Y") {
   archivo_zip <- gsub("\\\\","/",
